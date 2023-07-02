0553c408df1c53bfc38806a5ea1379d30ce83364
ncmendoza <nelzoncriollo@gmail.com>
2023-07-02 08:58:30 -0500

    se realizo unos cambios en reserva

--- gestioncitasmedicas/src/app/reservas/listar/page.tsx
+++ gestioncitasmedicas/src/app/reservas/listar/page.tsx
@@ -1,10 +1,10 @@
 import { ListarCitasComponent } from "../components/ListarCitas";
 
 export default function getReservas(){
+
     return(
 
         <div>
-            <h1>lista de reservas</h1>
             <ListarCitasComponent />
         </div>
     );
